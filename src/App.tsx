0362abc071a0d6d3a68c21d14f3e04886da29a7f
saibalmaiti <saibalmaiti23@gmail.com>
2026-08-22 13:33:51 +0530

    footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import Footer from "./components/romantic/Footer";
 
 import CreateBirthday from "./pages/CreateBirthday";
 import BirthdayCreated from "./pages/BirthdayCreated";
@@ -7,37 +14,45 @@ import BirthdayPage from "./pages/BirthdayPage";
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Default route */}
-        <Route
-          path="/"
-          element={<Navigate to="/create" replace />}
-        />
+      <div className="flex min-h-screen flex-col">
+        {/* Main page content */}
+        <div className="flex-1">
+          <Routes>
+            {/* Default route */}
+            <Route
+              path="/"
+              element={<Navigate to="/create" replace />}
+            />
 
-        {/* Create a new birthday surprise */}
-        <Route
-          path="/create"
-          element={<CreateBirthday />}
-        />
+            {/* Create a new birthday surprise */}
+            <Route
+              path="/create"
+              element={<CreateBirthday />}
+            />
 
-        {/* Page shown immediately after creation */}
-        <Route
-          path="/created/:slug"
-          element={<BirthdayCreated />}
-        />
+            {/* Page shown immediately after creation */}
+            <Route
+              path="/created/:slug"
+              element={<BirthdayCreated />}
+            />
 
-        {/* Actual shared birthday surprise */}
-        <Route
-          path="/b/:slug"
-          element={<BirthdayPage />}
-        />
+            {/* Actual shared birthday surprise */}
+            <Route
+              path="/b/:slug"
+              element={<BirthdayPage />}
+            />
 
-        {/* Temporary: edit route will be built next */}
-        <Route
-          path="*"
-          element={<Navigate to="/create" replace />}
-        />
-      </Routes>
+            {/* Temporary: edit route will be built next */}
+            <Route
+              path="*"
+              element={<Navigate to="/create" replace />}
+            />
+          </Routes>
+        </div>
+
+        {/* Common footer for every page */}
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 };
---
+++ src/components/romantic/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="border-t border-white/5 px-6 py-5 text-center">
+      <p className="text-xs text-white/30">
+        Made with <span className="text-pink-300">❤️</span> by Saibal
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
